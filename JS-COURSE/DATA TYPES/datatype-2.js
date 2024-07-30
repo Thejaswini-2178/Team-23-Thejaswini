3949aaf7871d49c9fa52257385654db41cc35439
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-07-30 13:30:48 +0530

    Datatypes

---
+++ JS-COURSE/DATA TYPES/datatype-2.js
@@ -0,0 +1,4 @@
+var a=24;
+var b=15;
+var c=25;
+document.write(a+b)
---
+++ JS-COURSE/DATA TYPES/datatype.js
@@ -0,0 +1,15 @@
+var m= 35;
+var n= 45;
+console.log(n+m);
+
+var name="Theju";
+var name="Gayathri";
+var name="Madhu";
+var name="jyothi";
+console.log("Theju"+"jyothi");
+
+var name="Theju";
+var place="Andhra";
+var mbl="2345678"
+var frontend_developer="yes";
+console.log("Theju");
