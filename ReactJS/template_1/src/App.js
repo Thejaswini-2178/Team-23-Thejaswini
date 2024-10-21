82a5fa1e7894ff6411a6ad0361987f5bce753c9f
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-21 22:52:09 +0530

    Templat

---
+++ ReactJS/template_1/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from './TEMPLATE-CREATION/Header';
+import Home from './TEMPLATE-CREATION/Home';
+import Footer from './TEMPLATE-CREATION/Footer';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
