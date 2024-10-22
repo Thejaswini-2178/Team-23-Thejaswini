561fbf6b2b34bf072dd3793aa5709b8d46f1a1bc
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-22 21:41:34 +0530

    Template_4

---
+++ ReactJS/template-4/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Footer from "./TEMPLATE-4/Footer";
+import Header from "./TEMPLATE-4/Header";
+import Home from "./TEMPLATE-4/Home";
+import Navbar from './TEMPLATE-4/Navbar';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/template-4/src/TEMPLATE-4/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer className="footer text-faded text-center py-5">
+                <div className="container"><p className="m-0 small">Copyright © Your Website 2023</p></div>
+            </footer>
+        </div>
+    )
+}
+export default Footer
