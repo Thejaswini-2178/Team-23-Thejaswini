9ce4129a6fa841b922602fbceb0366c485ad93a6
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-26 12:23:08 +0530

    Array

---
+++ JS-COURSE/ARRAY/assignments/assignment.js
@@ -0,0 +1,16 @@
+
+            var list=['apple', 'banana'] 
+            var list2=['cherry', 'date']
+            var list3=list.concat(list2)
+            console.log(list3)
+
+            var arr1 = [1, 2, 3];
+            var arr2 =[4, 5];
+            var arr3=[6, 7, 8];
+            var arr4 = arr1.concat(arr2).concat(arr3);
+            // var arr5=arr4.concat(arr3) 
+            console.log(arr4)
+
+            var a=['a','b']
+            var b=['c','d','e','f']
+            console.log(a.concat(b))
--- JS-COURSE/ARRAY/assignments/every..js
+++ JS-COURSE/ARRAY/assignments/every..js
@@ -1,12 +1,7 @@
-<html>
-    <head>
-        <title>EVERY-ASSIGNMENTS</title>
-    </head>
-    <body>
-        <!-- "How can you check if all elements in the array `[5, 10, 15, 20]` are divisible by 5?"
-        "How can you determine if every string in the array `['apple', 'banana', 'cherry']` contains the letter 'a'?"
-        "How can you verify that all numbers in `[3, 6, 9]` are less than 10 -->
-        <script>
+
+        // <!-- "How can you check if all elements in the array `[5, 10, 15, 20]` are divisible by 5?"
+        // "How can you determine if every string in the array `['apple', 'banana', 'cherry']` contains the letter 'a'?"
+        // "How can you verify that all numbers in `[3, 6, 9]` are less than 10 -->
             var numbers=[5,10,15,20]
             var check=numbers.every((n) =>{
                 return n%5==0;
@@ -26,6 +21,3 @@
                 return num<10
             })
             console.log(element)
-        </script>
-    </body>
-</html>
