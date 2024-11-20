eaca1c1c4ab0b2d180a79bd5850667fb6e32d858
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-20 20:59:27 +0530

    Template-2

---
+++ ReactJS/template_2/src/TEMPLATE_2_CREAT/Footer section/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            {/* Footer*/}
+            <footer className="bg-light py-5">
+                <div className="container px-4 px-lg-5"><div className="small text-center text-muted">Copyright © 2023 - Company Name</div></div>
+            </footer>
+        </div>
+    )
+}
