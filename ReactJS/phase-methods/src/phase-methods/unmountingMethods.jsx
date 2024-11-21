8caecbac7f2b4f1be51b86d5e319d9a2281e8995
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-21 22:24:19 +0530

    unmounting methods

--- ReactJS/phase-methods/src/phase-methods/unmountingMethods.jsx
+++ ReactJS/phase-methods/src/phase-methods/unmountingMethods.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class unmountingMethods extends Component {
   render() {
     return (
-      <div>unmountingMethods</div>
+      <div>
+        <h1>Hedding</h1>
+      </div>
     )
   }
 }
