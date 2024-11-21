2a71c4a89e36fb76c205b20af6258663360d6acd
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-21 22:13:16 +0530

    phases-methods

---
+++ ReactJS/phase-methods/src/phase-methods/mountingMethod.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class MountingExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: 'Hello, World!' };
+    console.log('Constructor: State initialized');
+  }
+
+static getDerivedStateFromProps(props, state) {
+    console.log('GetDerivedStateFromProps: Syncing props with state');
+    return null; // No change to state
+  }
+
+componentDidMount() {
+    console.log('ComponentDidMount: Component mounted in DOM');
+    setTimeout(() => {
+      this.setState({ message: 'React Lifecycle!' });
+    }, 2000);
+  }
+
+render() {
+    console.log('Render: Rendering UI');
+    return <h1>{this.state.message}</h1>;
+  }
+}
+
+export default MountingExample;
