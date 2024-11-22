2ab4f4aa8dcda5820d18e236bc1c4abac7a1cb27
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-22 22:14:46 +0530

    Context API

--- ReactJS/contextapi/src/FOLDER/Contextt API/Child.jsx
+++ ReactJS/contextapi/src/FOLDER/Contextt API/Child.jsx
@@ -1,9 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import NameContext from "./NameContext";
 
-export default class Child extends Component {
+class Child extends Component {
+  static contextType = NameContext; // Access the context using contextType
+
   render() {
+    const names = this.context;
+
     return (
-      <div>Child</div>
-    )
+      <div>
+        <h4>Child Component</h4>
+        <ul>
+          {names.map((person) => (
+            <li key={person.id}>{person.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
+
+export default Child;
