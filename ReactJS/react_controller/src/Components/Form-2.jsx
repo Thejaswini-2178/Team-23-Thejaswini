5f04607bc8d188c00d19c55884fb78f285af157a
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-24 23:22:21 +0530

    Form Handling

--- ReactJS/react_controller/src/Components/Form-2.jsx
+++ ReactJS/react_controller/src/Components/Form-2.jsx
@@ -9,7 +9,7 @@ class Form_2 extends Component {
       data: [],
     }
   }
-  
+
   //4.function for the handelClickSubmit
   handelClickSubmit = () => {
     console.log(this.state);              //this will dispalys the  entair state
@@ -17,30 +17,50 @@ class Form_2 extends Component {
     const newUser = [...this.state.data]    // copping the content in data
     newUser.push(this.state.user)         //adding that added data of user content 
     this.setState({ data: newUser })
+    this.handleClear();
   }
   handleInputFieldChange = (e) => {
     cons
     ole.log(e.target.name);
-    const newStateValus = { ...this.state.person };
+    const newStateValus = { ...this.state.user };
     newStateValus[e.target.name] = e.target.value;
-    this.setState({ person: newStateValus });
+    this.setState({ user: newStateValus });
     // this.setState({ fname: e.target.value });
+  };
+  handleClear = () => {
+    this.setState({
+      user: {
+        fname: "",
+        lname: "",
+        mail:"",
+        password:""
+      },
+    });
   };
   render() {
     return (
       //1. first step to create an general form
-      <form>
+ <div>
+       <form>
         <label htmlFor=""> First Name</label>
-        <input type="text" id="fname" name="fname" /><br />
+        <input type="text" id="fname" name="fname" value={this.user.fname} onChange={this.handleInputFieldChange} /> {" "}<br />
         <label htmlFor=""> Last Name</label>
-        <input type="text" id="lname" name='lname' /><br />
+        <input type="text" id="lname" name='lname' value={this.user.lname} onChange={this.handleInputFieldChange} /> {" "}<br />
         <label htmlFor="">Email</label>
-        <input type="mail" id="mail" name="name" /><br />
+        <input type="mail" id="mail" name="name"value={this.user.mail} onChange={this.handleInputFieldChange} /> {" "} <br />
         <label htmlFor="">Password</label>
-        <input type="password" id="password" name="password" /><br />
+        <input type="password" id="password" name="password" value={this.user.password} onChange={this.handleInputFieldChange} /> {" "}<br />
         {/* 2 we have to write th efunction to the user button*/}
         <button type="button" id="button" onClick={this.handelClickSubmit} >SUBMIT</button>
       </form>
+      <hr/>
+      {this.state.data.map((usr)=>{
+        return (
+          <li>{usr.fname}-{uxr.password}</li>
+      )
+      })}
+ </div>
+ 
     )
   }
 }
