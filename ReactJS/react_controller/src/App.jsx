08035ade4b87127f950a66dbf92b7f3a84227727
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-24 21:52:03 +0530

    Form handling

--- ReactJS/react_controller/src/App.jsx
+++ ReactJS/react_controller/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import Form from './Components/form'
+// import Form from './Components/form'
+import Form_2 from './Components/Form-2'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Form/>
+      {/* <Form/> */}
+      <Form_2/>
     </>
   )
 }
