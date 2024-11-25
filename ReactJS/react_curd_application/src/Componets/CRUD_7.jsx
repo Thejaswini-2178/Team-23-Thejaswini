4b0b98955baa1e6cbc6571022d33c2e15f84a1f6
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-25 22:37:43 +0530

    CRUD_Application_REACT

---
+++ ReactJS/react_curd_application/src/Componets/CRUD_7.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class RegistrationForm6 extends Component {
+    render() {
+      return (
+        <div>
+          <h3>Registration Form 6</h3>
+          <form>
+            <div>
+              <label>Enter a Color</label>
+              <input type="color" />
+            </div>
+            <div>
+              <label>Select Range</label>
+              <input type="range" />
+            </div>
+            <div>
+              <label>Preferred Contact Method</label>
+              <input type="radio" name="contact" value="email" /> Email
+              <input type="radio" name="contact" value="phone" /> Phone
+            </div>
+            <div>
+              <label>Start Time</label>
+              <input type="time" />
+            </div>
+          </form>
+        </div>
+      );
+    }
+  }
