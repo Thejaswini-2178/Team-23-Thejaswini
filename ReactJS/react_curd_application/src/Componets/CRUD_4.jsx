9c3ce976b7664b4459b0aaf4df2a45a053836311
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-26 22:50:37 +0530

    CRUD

--- ReactJS/react_curd_application/src/Componets/CRUD_4.jsx
+++ ReactJS/react_curd_application/src/Componets/CRUD_4.jsx
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
 
 class RegistrationForm3 extends Component {
-    render() {
-      return (
-        <div>
-          <h3>Registration Form 3</h3>
-          <form>
-            <div>
-              <label>First Name</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>Last Name</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>Gender</label>
-              <select>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-              </select>
-            </div>
-            <div>
-              <label>Website</label>
-              <input type="url" />
-            </div>
-          </form>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <h3>Registration Form 3</h3>
+        <form>
+          <div>
+            <label>First Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Last Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Gender</label>
+            <select>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+          </div>
+          <div>
+            <label>Website</label>
+            <input type="url" />
+          </div>
+        </form>
+      </div>
+    );
   }
+}
