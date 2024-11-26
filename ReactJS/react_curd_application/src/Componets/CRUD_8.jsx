d219b49393a7578a4b26d218545886cedfd12cfb
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-26 22:51:03 +0530

    CRUD

--- ReactJS/react_curd_application/src/Componets/CRUD_8.jsx
+++ ReactJS/react_curd_application/src/Componets/CRUD_8.jsx
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
 
 class RegistrationForm7 extends Component {
-    render() {
-      return (
-        <div>
-          <h3>Registration Form 7</h3>
-          <form>
-            <div>
-              <label>Twitter Handle</label>
-              <input type="text" placeholder="@username" />
-            </div>
-            <div>
-              <label>Upload Profile Image</label>
-              <input type="file" />
-            </div>
-            <div>
-              <label>Newsletter Subscription</label>
-              <input type="checkbox" />
-            </div>
-            <div>
-              <label>Country</label>
-              <select>
-                <option value="usa">USA</option>
-                <option value="india">India</option>
-                <option value="uk">UK</option>
-              </select>
-            </div>
-          </form>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <h3>Registration Form 7</h3>
+        <form>
+          <div>
+            <label>Twitter Handle</label>
+            <input type="text" placeholder="@username" />
+          </div>
+          <div>
+            <label>Upload Profile Image</label>
+            <input type="file" />
+          </div>
+          <div>
+            <label>Newsletter Subscription</label>
+            <input type="checkbox" />
+          </div>
+          <div>
+            <label>Country</label>
+            <select>
+              <option value="usa">USA</option>
+              <option value="india">India</option>
+              <option value="uk">UK</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    );
   }
+}
