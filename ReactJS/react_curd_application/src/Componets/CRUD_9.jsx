3c8f78e86913a32fda3759839a4ae51ef0dfd954
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-26 22:51:13 +0530

    CRUD

--- ReactJS/react_curd_application/src/Componets/CRUD_9.jsx
+++ ReactJS/react_curd_application/src/Componets/CRUD_9.jsx
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
 
 class RegistrationForm8 extends Component {
-    render() {
-      return (
-        <div>
-          <h3>Registration Form 8</h3>
-          <form>
-            <div>
-              <label>Full Name</label>
-              <input type="text" />
-            </div>
-            <div>
-              <label>Start Date</label>
-              <input type="date" />
-            </div>
-            <div>
-              <label>Experience Level</label>
-              <select>
-                <option value="beginner">Beginner</option>
-                <option value="intermediate">Intermediate</option>
-                <option value="expert">Expert</option>
-              </select>
-            </div>
-            <div>
-              <label>Password</label>
-              <input type="password" />
-            </div>
-          </form>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <h3>Registration Form 8</h3>
+        <form>
+          <div>
+            <label>Full Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Start Date</label>
+            <input type="date" />
+          </div>
+          <div>
+            <label>Experience Level</label>
+            <select>
+              <option value="beginner">Beginner</option>
+              <option value="intermediate">Intermediate</option>
+              <option value="expert">Expert</option>
+            </select>
+          </div>
+          <div>
+            <label>Password</label>
+            <input type="password" />
+          </div>
+        </form>
+      </div>
+    );
   }
+}
