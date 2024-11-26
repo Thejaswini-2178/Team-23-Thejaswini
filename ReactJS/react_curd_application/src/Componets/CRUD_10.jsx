193c742c09a587abdf342a9a178b86ba759b70cb
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-26 22:51:45 +0530

    CRUD

--- ReactJS/react_curd_application/src/Componets/CRUD_10.jsx
+++ ReactJS/react_curd_application/src/Componets/CRUD_10.jsx
@@ -8,29 +8,29 @@ export default class CRUD_10 extends Component {
   }
 }
 class RegistrationForm9 extends Component {
-    render() {
-      return (
-        <div>
-          <h3>Registration Form 9</h3>
-          <form>
-            <div>
-              <label>Enter Feedback</label>
-              <textarea rows="4" />
-            </div>
-            <div>
-              <label>Upload Supporting Documents</label>
-              <input type="file" />
-            </div>
-            <div>
-              <label>Rate Us</label>
-              <input type="range" />
-            </div>
-            <div>
-              <label>Agreement</label>
-              <input type="checkbox" /> I agree to terms.
-            </div>
-          </form>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <h3>Registration Form 9</h3>
+        <form>
+          <div>
+            <label>Enter Feedback</label>
+            <textarea rows="4" />
+          </div>
+          <div>
+            <label>Upload Supporting Documents</label>
+            <input type="file" />
+          </div>
+          <div>
+            <label>Rate Us</label>
+            <input type="range" />
+          </div>
+          <div>
+            <label>Agreement</label>
+            <input type="checkbox" /> I agree to terms.
+          </div>
+        </form>
+      </div>
+    );
   }
+}
