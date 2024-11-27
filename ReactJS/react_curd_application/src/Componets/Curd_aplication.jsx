b52aa257c839cbe16b9d89fa76c60c500f65b217
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-27 18:23:33 +0530

    CRUD

--- ReactJS/react_curd_application/src/Componets/Curd_aplication.jsx
+++ ReactJS/react_curd_application/src/Componets/Curd_aplication.jsx
@@ -1,232 +1,132 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-export default class RegistrationForm extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      person: {
-        username: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
+export default class Curd_aplication extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      user:{
+        name:"",contact:"",age:"",gender:"",fatherName:"",motherName:"",email:"",password:""
       },
-      editIndex: null,
-      persons: [
-        {
-          username: "john_doe",
-          email: "john.doe@example.com",
-          password: "password123",
-          confirmPassword: "password123",
-        },
-        {
-          username: "jane_smith",
-          email: "jane.smith@example.com",
-          password: "qwerty456",
-          confirmPassword: "qwerty456",
-        },
-        {
-          username: "michael_brown",
-          email: "michael.brown@example.com",
-          password: "abc123",
-          confirmPassword: "abc123",
-        },
-        {
-          username: "lisa_white",
-          email: "lisa.white@example.com",
-          password: "securepass789",
-          confirmPassword: "securepass789",
-        },
-        {
-          username: "david_jones",
-          email: "david.jones@example.com",
-          password: "mypassword321",
-          confirmPassword: "mypassword321",
-        },
-      ],
-    };
+      userData:[]
+    }
+  }
+  
+  onChangeEventHandling=(e)=>{
+    console.log(e.target.name)
+    var inputChange={...this.state.user}
+    inputChange[e.target.name]=e.target.value
+    this.setState({
+      user:inputChange
+    })
   }
 
-  handleChange = (e) => {
-    console.log(e.target.name);
-    const newPerson = { ...this.state.person };
-    newPerson[e.target.name] = e.target.value;
-    this.setState({ person: newPerson });
-  };
+  onSubmit=(event)=>{
+    event.preventDefault()
+    console.log(this.state)
+    const onSubmit=[...this.state.userData]
+    onSubmit.push(this.state.user)
+    this.setState({
+      userData:onSubmit
+    })
+    console.log(this.state.userData)
+  }
 
-  registerUser = () => {
-    const newPersons = [...this.state.persons];
-    newPersons.push(this.state.person);
-    this.setState({ persons: newPersons });
-    this.clearForm();
-  };
-
-  clearForm = () => {
-    let newPerson = { ...this.state.person };
-    newPerson = {
-      username: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-    };
-    this.setState({ person: newPerson });
-  };
-
-  handleDelete = (i) => {
-    console.log(i);
-    // const newPersons = this.state.persons.filter((obj,index)=>{
-    //     return index !== i
-    // })
-    // this.setState({persons :newPersons })
-
-    const newPersons = [...this.state.persons];
-    newPersons.splice(i, 1);
-    this.setState({ persons: newPersons });
-  };
-
-  handleEdit = (per, i) => {
-    this.setState({ editIndex: i, person: per });
-  };
-
-  updateUser = () => {
-    const newPersons = [...this.state.persons];
-    newPersons[this.state.editIndex] = this.state.person;
-    this.setState({ persons: newPersons, editIndex: null });
-    this.clearForm()
-  };
   render() {
     return (
-      <div
-        style={{
-          padding: "20px",
-          display: "flex",
-          justifyContent: "space-around",
-        }}
-      >
-        <div id="userform" style={{ width: "40%" }}>
-          <h2>Registration Form</h2>
-          <form>
-            <div style={{ marginBottom: "10px" }}>
-              <label>Username</label>
-              <input
-                type="text"
-                name="username"
-                style={{ width: "100%", padding: "8px", marginBottom: "5px" }}
-                value={this.state.person.username}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div style={{ marginBottom: "10px" }}>
-              <label>Email</label>
-              <input
-                type="email"
-                name="email"
-                style={{ width: "100%", padding: "8px", marginBottom: "5px" }}
-                value={this.state.person.email}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div style={{ marginBottom: "10px" }}>
-              <label>Password</label>
-              <input
-                type="password"
-                name="password"
-                style={{ width: "100%", padding: "8px", marginBottom: "5px" }}
-                value={this.state.person.password}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div style={{ marginBottom: "10px" }}>
-              <label>Confirm Password</label>
-              <input
-                type="password"
-                name="confirmPassword"
-                style={{ width: "100%", padding: "8px", marginBottom: "5px" }}
-                value={this.state.person.confirmPassword}
-                onChange={this.handleChange}
-              />
-            </div>
-            {this.state.editIndex != null ? (
-              <>
-                <button
-                  type="button"
-                  style={{
-                    backgroundColor: "#007BFF",
-                    color: "#fff",
-                    padding: "10px",
-                    border: "none",
-                    cursor: "pointer",
-                  }}
-                  onClick={this.updateUser}
-                >
-                  Update
-                </button>
-              </>
-            ) : (
-              <button
-                type="button"
-                style={{
-                  backgroundColor: "#007BFF",
-                  color: "#fff",
-                  padding: "10px",
-                  border: "none",
-                  cursor: "pointer",
-                }}
-                onClick={this.registerUser}
-              >
-                Register
-              </button>
-            )}
-          </form>
+      <div>
+        <div className='divdata1'>
+        <form >
+        <h2>Registration Form</h2>
+          <label htmlFor="name">Name:</label><br />
+          <input type="text" 
+          id="name" 
+          value={this.state.user.name}
+         onChange={this.onChangeEventHandling}
+         name="name" required /><br /><br />
+
+          <label htmlFor="contact">Contact Number:</label><br />
+          <input type="tel" 
+          id="contact" 
+          value={this.state.user.contact}
+          onChange={this.onChangeEventHandling}
+          name="contact" required /><br /><br />
+
+          <label htmlFor="age">Age:</label><br />
+          <input type="number" 
+          id="age" 
+          value={this.state.user.age}
+          onChange={this.onChangeEventHandling}
+          name="age" /><br /><br />
+
+          <label>Gender:</label><br />
+          <input type="radio" 
+          id="male" 
+          value="male"
+          onChange={this.onChangeEventHandling}
+          checked={this.state.user.gender === 'male'}
+          name="gender"
+          /> Male <br />
+          <input type="radio" 
+          id="female" 
+          value="female"
+          onChange={this.onChangeEventHandling}
+          checked={this.state.user.gender === 'female'}
+          name="gender" 
+          />Female <br /><br />
+
+          <label htmlFor="fatherName">Father's Name:</label><br />
+          <input type="text" 
+          id="fatherName" 
+          value={this.state.user.fatherName}
+          onChange={this.onChangeEventHandling}
+          name="fatherName" required /><br /><br />
+
+          <label htmlFor="motherName">Mother's Name:</label><br />
+          <input type="text" 
+          id="motherName" 
+          value={this.state.user.motherName}
+          onChange={this.onChangeEventHandling}
+          name="motherName" required /><br /><br />
+
+          <label htmlFor="email">Email:</label><br />
+          <input type="email" 
+          id="email" 
+          value={this.state.user.email}
+          onChange={this.onChangeEventHandling}
+          name="email" required /><br /><br />
+
+          <label htmlFor="password">Password:</label><br />
+          <input type="password" 
+          id="password" 
+          value={this.state.user.password}
+          onChange={this.onChangeEventHandling}
+          name="password" required /><br /><br />
+          <button type="submit" onClick={this.onSubmit}>Submit</button>
+
+        </form>
         </div>
-        <div id="usertable" style={{ width: "50%" }}>
-          <table
-            border="1"
-            style={{ width: "100%", marginTop: "20px", textAlign: "left" }}
-          >
+        <div className='divdata2'>
+          <table border={1} cellPadding={10} cellSpacing={0}>
             <thead>
               <tr>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Confirm Password</th>
-                <th>Edit</th>
-                <th>Delete</th>
+              <th>Name</th>
+              <th>Contact</th>
+              <th>Age</th>
+              <th>Gender</th>
+              <th>FatherName</th>
+              <th>MotherName</th>
+              <th>Mail</th>
+              <th>Password</th>
               </tr>
             </thead>
             <tbody>
-              {this.state.persons.map((per, i) => {
-                return (
-                  <tr>
-                    <td>{per.username}</td>
-                    <td>{per.email}</td>
-                    <td>{per.password}</td>
-                    <td>{per.confirmPassword}</td>
-                    <td>
-                      <button
-                        onClick={() => {
-                          this.handleEdit(per, i);
-                        }}
-                      >
-                        Edit
-                      </button>
-                    </td>
-                    <td>
-                      <button
-                        onClick={() => {
-                          this.handleDelete(i);
-                        }}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
+              <tr></tr>
             </tbody>
           </table>
+
+
         </div>
       </div>
-    );
+    )
   }
 }
