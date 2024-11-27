cbdc9f563b4281d9d01cc2cbf0e9fdea32e71a67
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-27 12:12:16 +0530

    Form handling

--- ReactJS/react_controller/src/App.jsx
+++ ReactJS/react_controller/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-// import Form from './Components/form'
-import Form_2 from './Components/Form-2'
-import Form3 from './Components/Form3'
-import Form4 from './Components/Form4'
+// import Form from './Components/Form'
+// import Form_2 from './Components/Form-2'
+// import Form3 from './Components/Form3'
+// import Form4 from './Components/Form4'
+import Form5 from './Components/Form5'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +14,8 @@ function App() {
       {/* <Form/> */}
       {/* <Form_2/> */}
       {/* <Form3/> */}
-      <Form4/>
+      {/* <Form4/> */}
+      <Form5/>
     </>
   )
 }
---
+++ ReactJS/react_controller/src/Components/Form5.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+
+export class Form5 extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: {
+                fname: "", email: "", phone: "", password: "", confirmPassword: ""
+            },
+            userData: []
+        }
+    }
+    onChangeEvent = (e) => {
+        console.log(e.target.name)
+        // coping the data
+        var addData = { ...this.state.data }
+        // updating th edata
+        addData[e.target.name] = e.target.value
+        this.setState({
+            data: addData
+        })
+    }
+
+    submitForm = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+        // opping the data
+        var submittedData = [...this.state.userData]
+        submittedData.push(this.state.data)
+        this.setState({
+            userData: submittedData
+        })
+        this.clearForm()
+    }
+    
+
+    clearForm = () => {
+        this.setState = {
+            data: {
+                fname: "", email: "", phone: "", password: "", confirmPassword: ""
+            },
+        }
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <div style={{ marginBottom: 15 }}>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text"
+                            id="fname"
+                            name="fname"
+                            value={this.state.data.fnamename}
+                            onChange={this.onChangeEvent}
+                            style={{ width: '100%', padding: 8, marginTop: 5 }} />
+
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email"
+                            name="email"
+                            value={this.state.data.email}
+                            onChange={this.onChangeEvent}
+                            style={{ width: '100%', padding: 8, marginTop: 5 }} />
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <label htmlFor="phone">Phone Number:</label>
+                        <input type="tel" id="phone"
+                            value={this.state.data.phone}
+                            name="phone"
+                            onChange={this.onChangeEvent}
+                            style={{ width: '100%', padding: 8, marginTop: 5 }} />
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" id="password"
+                            name="password"
+                            value={this.state.data.password}
+                            style={{ width: '100%', padding: 8, marginTop: 5 }}
+                            onChange={this.onChangeEvent}
+                        />
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <label htmlFor="confirmPassword">Confirm Password:</label>
+                        <input type="password" id="confirmPassword"
+                            name="confirmPassword"
+                            value={this.state.data.confirmPassword}
+                            style={{ width: '100%', padding: 8, marginTop: 5 }}
+                            onChange={this.onChangeEvent}
+                        />
+                    </div>
+                    <button type="submit"
+                        onClick={this.submitForm}
+                        style={{ padding: '10px 20px', backgroundColor: '#007bff', color: '#fff', border: 'none', cursor: 'pointer' }}>
+                        Register
+                    </button>
+                </form>
+                <ul>
+                    {this.state.userData.map((val, index) => (
+                        <li key={index}>
+                            Name: {val.fname} Email: {val.email}, Phone: {val.phone}, Password: {val.password}, Confirm Password: {val.confirmPassword}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Form5
