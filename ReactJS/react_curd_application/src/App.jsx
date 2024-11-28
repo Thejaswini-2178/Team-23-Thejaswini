265687a0d7021d8887687fcb264ea9efb18e9777
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-28 19:44:42 +0530

    CURD

--- ReactJS/react_curd_application/src/App.jsx
+++ ReactJS/react_curd_application/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import Curd_aplication from './Componets/Curd_aplication'
+// import Curd_aplication from './Componets/Curd_aplication'
+import RegistrationForm1 from './Componets/CRUD_2'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Curd_aplication/>
+    {/* <Curd_aplication/> */}
+    <RegistrationForm1/>
     </>
   )
 }
