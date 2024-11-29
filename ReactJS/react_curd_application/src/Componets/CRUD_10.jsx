428d96b365b4f314e1047781328e0cdc87acd07d
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-29 23:00:02 +0530

    CRUD

--- ReactJS/react_curd_application/src/Componets/CRUD_10.jsx
+++ ReactJS/react_curd_application/src/Componets/CRUD_10.jsx
@@ -1,36 +1,30 @@
 import React, { Component } from 'react'
 
-export default class CRUD_10 extends Component {
-  render() {
-    return (
-      <div>CRUD_10</div>
-    )
-  }
-}
-class RegistrationForm9 extends Component {
+ class CRUD10 extends Component {
   render() {
     return (
       <div>
-        <h3>Registration Form 9</h3>
-        <form>
-          <div>
-            <label>Enter Feedback</label>
-            <textarea rows="4" />
-          </div>
-          <div>
-            <label>Upload Supporting Documents</label>
-            <input type="file" />
-          </div>
-          <div>
-            <label>Rate Us</label>
-            <input type="range" />
-          </div>
-          <div>
-            <label>Agreement</label>
-            <input type="checkbox" /> I agree to terms.
-          </div>
-        </form>
-      </div>
-    );
+      <h3>Registration Form 9</h3>
+      <form>
+        <div>
+          <label>Enter Feedback</label>
+          <textarea rows="4" />
+        </div>
+        <div>
+          <label>Upload Supporting Documents</label>
+          <input type="file" />
+        </div>
+        <div>
+          <label>Rate Us</label>
+          <input type="range" />
+        </div>
+        <div>
+          <label>Agreement</label>
+          <input type="checkbox" /> I agree to terms.
+        </div>
+      </form>
+    </div>
+    )
   }
 }
+export default CRUD10
