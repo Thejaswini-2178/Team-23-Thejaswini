ff260eed0eaba9c1b146b9b41e32e64456bdee53
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-30 22:45:07 +0530

    Conditional Rendering

---
+++ ReactJS/conditional/src/Conditionalrendering/Ifwithlogic.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Ifwithlogic extends Component {
+    render() {
+          const hasMessages = true;
+          return (
+            <div>
+              <h1>Inbox</h1>
+              {hasMessages && <p>You have new messages.</p>}
+            </div>
+          );
+        }
+      }
+      
