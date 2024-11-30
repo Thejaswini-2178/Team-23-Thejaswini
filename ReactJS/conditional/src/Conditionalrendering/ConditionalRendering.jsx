f08697bee8ac9b1c70563d19efadb82da81ee910
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-30 22:44:51 +0530

    Conditional Rendering

---
+++ ReactJS/conditional/src/Conditionalrendering/ConditionalRendering.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class ConditionalRendering extends Component {
+        state = { isLoggedIn: false };
+      
+        toggleLogin = () => {
+          this.setState({ isLoggedIn: !this.state.isLoggedIn });
+        };
+      
+        render() {
+          return (
+            <div>
+              <button onClick={this.toggleLogin}>
+                {this.state.isLoggedIn ? "Logout" : "Login"}
+              </button>
+              {this.state.isLoggedIn ? <h1>Welcome!</h1> : <h1>Please log in.</h1>}
+            </div>
+          );
+        }
+      }
+  
+      
