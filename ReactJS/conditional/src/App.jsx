e280349999587f82915e3e6ac179b97e22debfbb
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-30 22:44:44 +0530

    Conditional Rendering

--- ReactJS/conditional/src/App.jsx
+++ ReactJS/conditional/src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 import Ifelse from './Conditionalrendering/Ifelse'
+import Logical from './Conditionalrendering/Logical'
+import Ifwithlogic from './Conditionalrendering/Ifwithlogic'
+import Switch from './Conditionalrendering/Switch'
+import RenderCondition from './Conditionalrendering/RenderCondition'
+import RenderingComponents from './Conditionalrendering/RenderingComponents'
+import ConditionalRendering from './Conditionalrendering/ConditionalRendering'
+import Rendering from './Conditionalrendering/Rendering'
+import Renderingnull from './Conditionalrendering/Renderingnull'
+import { NulltoRendering } from './Conditionalrendering/Nullto Rendering'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Ifelse/>       
+    <Ifelse/> 
+    <Logical/>
+    <Ifwithlogic/>
+    <Switch/>
+    <RenderCondition/>
+    <RenderingComponents/>
+    <ConditionalRendering/>
+    <Array/>
+    <Rendering/>
+    <Renderingnull/>
+    <NulltoRendering/>
     </>
   )
 }
