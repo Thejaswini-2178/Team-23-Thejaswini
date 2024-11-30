bce078a3617021c2c209bde6cb03f1ee392451fd
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-11-30 22:45:27 +0530

    Conditional Rendering

---
+++ ReactJS/conditional/src/Conditionalrendering/RenderCondition.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class RenderCondition extends Component {
+        renderContent() {
+          const isLoggedIn = true;
+          if (isLoggedIn) {
+            return <h1>Welcome, User!</h1>;
+          }
+          return <h1>Please log in.</h1>;
+        }
+      
+        render() {
+          return <div>{this.renderContent()}</div>;
+        }
+      }
+      
