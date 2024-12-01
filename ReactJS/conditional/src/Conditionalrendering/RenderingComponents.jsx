8e334cf9f283e08d5a197616777f434865255710
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-12-01 21:20:37 +0530

    Conditional Rendering

--- ReactJS/conditional/src/Conditionalrendering/RenderingComponents.jsx
+++ ReactJS/conditional/src/Conditionalrendering/RenderingComponents.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 
 export default class RenderingComponents extends Component {
-        render() {
-          return <h1>Admin Dashboard</h1>;
-        }
-      }
-      
-      class User extends React.Component {
-        render() {
-          return <h1>User Dashboard</h1>;
-        }
-      }
-      
-      class App extends React.Component {
-        render() {
-          const isAdmin = true;
-          return isAdmin ? <Admin /> : <User />;
-        }
-      }
-      
+  render() {
+    return <h1>Admin Dashboard</h1>;
+  }
+}
+
+class User extends React.Component {
+  render() {
+    return <h1>User Dashboard</h1>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    const isAdmin = true;
+    return isAdmin ? <Admin /> : <User />;
+  }
+}
+
