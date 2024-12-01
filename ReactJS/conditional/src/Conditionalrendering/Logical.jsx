d392279e3a6468e193a64ac2460d9b3c4b25d21c
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-12-01 21:20:16 +0530

    Conditional Rendering

--- ReactJS/conditional/src/Conditionalrendering/Logical.jsx
+++ ReactJS/conditional/src/Conditionalrendering/Logical.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 
 export default class Logical extends Component {
-        render() {
-          const isLoggedIn = true;
-          return isLoggedIn ? <h1>Welcome, User!</h1> : <h1>Please log in.</h1>;
-        }
-      }
-      
+  render() {
+    const isLoggedIn = true;
+    return isLoggedIn ? <h1>Welcome, User!</h1> : <h1>Please log in.</h1>;
+  }
+}
+
