d010220eefa17debf962d14052557f9373f76b89
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-12-01 21:20:30 +0530

    Conditional Rendering

--- ReactJS/conditional/src/Conditionalrendering/Rendering.jsx
+++ ReactJS/conditional/src/Conditionalrendering/Rendering.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 
 export default class Rendering extends Component {
-        render() {
-          const showDetails = true;
-          return (
-            <>
-              <h1>Welcome to the app</h1>
-              {showDetails && <p>Here are the details...</p>}
-            </>
-          );
-        }
-      }
-      
+  render() {
+    const showDetails = true;
+    return (
+      <>
+        <h1>Welcome to the app</h1>
+        {showDetails && <p>Here are the details...</p>}
+      </>
+    );
+  }
+}
