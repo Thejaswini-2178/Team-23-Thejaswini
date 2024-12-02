4d450ba9a681300d0edf8e5d103894ae675e3317
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-12-02 22:26:13 +0530

    Object_values_non modified

---
+++ ReactJS/datatypes_functionalcomponents/src/Folder/ObjectValues.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ObjectValues() {
+    const obj = { a: 1, b: 2, c: 3 };
+    const values = Object.values(obj);
+  return (
+    <div>
+        <p>console.log(values)</p>
+    </div>
+  )
+}
+
+export default ObjectValues
