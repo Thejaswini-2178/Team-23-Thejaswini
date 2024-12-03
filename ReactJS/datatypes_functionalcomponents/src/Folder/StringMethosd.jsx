9de23997b79039396da57d8bdd80e2c595af5abf
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-12-03 23:17:18 +0530

    StringMethods

--- ReactJS/datatypes_functionalcomponents/src/Folder/StringMethosd.jsx
+++ ReactJS/datatypes_functionalcomponents/src/Folder/StringMethosd.jsx
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
+import React from 'react'
 
-function StringMethodsExample() {
-  const [input, setInput] = useState("Hello World!");
-
-  const handleUppercase = () => setInput(input.toUpperCase());
-  const handleLowercase = () => setInput(input.toLowerCase());
-  const handleTrim = () => setInput(input.trim());
-  const handleReplace = () => setInput(input.replace("World", "React"));
-
-  return (
+export function StringMethods(){
+  var str1="hii this is thejaswini"
+  var str2="      hellow this this is the thejaswini iam from mpl      "
+  
+  return(
     <div>
-      <h1>String Methods in React</h1>
-      <p>{input}</p>
-      <button onClick={handleUppercase}>To Uppercase</button>
-      <button onClick={handleLowercase}>To Lowercase</button>
-      <button onClick={handleTrim}>Trim</button>
-      <button onClick={handleReplace}>Replace "World" with "React"</button>
+      <p>{str1.length}</p>
+      <p>{str2.length}</p>
+      <p>{str2.trim()}</p>
+      {/* <p>{str2.join(-)}</p> */}
+      <p>{str1.toLocaleLowerCase()}</p>
+      <p>{str1.toLocaleUpperCase()}</p>
+      <p>{str2.charCodeAt(5)}</p>
+      <p>{str1.charAt(12)}</p>
+      <p>{str1.substring(2,5)}</p>  {/* it will gives the from index to n-1 index of str */}
+      <p>subsrt:{str2.substr(12,7)}</p>
+      <p>subsrt:{str2.substr(-12,7)}</p>
+      <p>slice:{str1.slice(2,7)}</p>
+      <p>substring:{str1.substring(2,7)}</p>
+      <p>split method:{str1.split(":")}</p>
+      
+      <h1>split</h1>
+      <p>{str1.split(' ').reverse().join(' ')}</p>
+      <p>{str1.split('').reverse().join('')}</p>
+      <p>{str1.split(",").reverse().join("")}</p>
+      <p>{str2.split('t').length-1}</p>
+      <p>{str2.split('s').length-1}</p>
+      <p>{str1.split('t')}</p>
     </div>
-  );
+  )
 }
-
-export default StringMethodsExample;
