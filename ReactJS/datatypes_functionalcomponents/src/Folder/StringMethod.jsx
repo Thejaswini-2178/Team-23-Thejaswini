98ea1fcaede64c89b18b6e95862f5f42552551bd
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-12-04 21:01:03 +0530

    StringMethods

--- ReactJS/datatypes_functionalcomponents/src/Folder/StringMethod.jsx
+++ ReactJS/datatypes_functionalcomponents/src/Folder/StringMethod.jsx
@@ -3,17 +3,38 @@ import React from 'react';
 function Str1() {
     var str = "hellow_world";
     var palan="hellow"
-    var char="javascript"
+    const char="javascript"
+    var word="swiss"
     return (
         <div>
             <p>{str.split("_").reverse().join(" ")}</p>
             <p>{palan.split("").reverse().join("")===palan?"true":"false"}</p>
-            <h1>finding th emost frequent character</h1>
-            <p></p>
-            <h2>Removing  th eduplicate characters</h2>
-            <p>{char.split("").filter().indexOf()}</p>
+            <h1>Removing the duplicate characteristics</h1>
+            <p>{char.split("").filter((val,i)=>char.indexOf(val)===i).join("")}</p>
+            <h2>Now finding the first non repeating character</h2>
+            <p>
+                {
+                    (()=>{
+                         const charCount={};
+                         word.split("").forEach(val=>charCount[val]=(charCount[val]||0)+1);
+                         return word.split("").find(char=>charCount[char]===1)
+                    })()
+                }
+            </p>
+            <h2>Most frequently appering character</h2>
+            <h2>Palndrome or not</h2>
+            <p>{
+                (()=>{
+                    const a="madam";
+                    const b="hello";
+                    return (a.split("").reverse().join("")===a?"true":"false")
+                })()
+                }
+            </p>
+            <h3>String appears in count</h3>
+            <p>{str.split("").filter((val,i)=>val===i).length}</p>
+           
         </div>
     );
 }
-
 export default Str1;
