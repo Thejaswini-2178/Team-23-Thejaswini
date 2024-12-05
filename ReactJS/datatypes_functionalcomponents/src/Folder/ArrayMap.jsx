d4b1393bafa06dd7106d9310b60a0779914dca14
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-12-05 22:28:38 +0530

    ArrayMethod

--- ReactJS/datatypes_functionalcomponents/src/Folder/ArrayMap.jsx
+++ ReactJS/datatypes_functionalcomponents/src/Folder/ArrayMap.jsx
@@ -1,1 +1,20 @@
+import React, { useState } from "react";
+
+function PushExample() {
+  const [items, setItems] = useState(["Apple", "Banana"]);
+
+  const handleAdd = () => {
+    const newItems = [...items];
+    newItems.push("Cherry");
+    setItems(newItems);
+  };
+  return (
+    <div>
+      <h3>Modified Method: push()</h3>
+      <p>{items.join(", ")}</p>
+      <button onClick={handleAdd}>Add Cherry</button>
+    </div>
+  );
+}
+export default PushExample;
 
