7b4750d8ec14fa3c52af45022512a028bdb7d262
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-12-05 22:30:28 +0530

    ObjectMethod

--- ReactJS/datatypes_functionalcomponents/src/Folder/ObjectUpdated_assign.jsx
+++ ReactJS/datatypes_functionalcomponents/src/Folder/ObjectUpdated_assign.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function ObjectUpdated_assign() {
     const target = { a: 1 };
     const source = { b: 2, c: 3 };
-    
     return (<div>
     <p>Object.assign(target, source)</p>
     console.log(target)
