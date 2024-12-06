adcf91119d1403162fba27ae88f06e6043fee46b
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-12-06 21:35:05 +0530

    CRUD

--- ReactJS/react_curd_application/src/Componets/CRUD_4.jsx
+++ ReactJS/react_curd_application/src/Componets/CRUD_4.jsx
@@ -1,110 +1,110 @@
 import React, { Component } from 'react'
 
 class RegistrationForm3 extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-     user:{
-      fname:"",lname:"",gender:"",url:""
-    },
-    data:[]
+    this.state = {
+      user: {
+        fname: "", lname: "", gender: "", url: ""
+      },
+      data: []
     }
   }
-  onChangeEvent=(e)=>{
+  onChangeEvent = (e) => {
     console.log(e.target.name)
-    const event={...this.state.user}
-    event[e.target.name]=e.target.value
+    const event = { ...this.state.user }
+    event[e.target.name] = e.target.value
     this.setState({
-      user:event
+      user: event
     })
   }
-  submitButton=()=>{
+  submitButton = () => {
     console.log(this.state)
-    const submitData=[...this.state.data]
+    const submitData = [...this.state.data]
     submitData.push(this.state.user)
     this.setState({
-      data:submitData
+      data: submitData
     })
     this.clearForm()
   }
-  editIndex=null
-  editButtonEvent=((val,i)=>{
+  editIndex = null
+  editButtonEvent = ((val, i) => {
     console.log(i)
-    this.setState({editIndex:i,user:val})
+    this.setState({ editIndex: i, user: val })
   })
-  deleteButtonEvent=(i)=>{
+  deleteButtonEvent = (i) => {
     console.log(i)
-    const delbtn=[...this.state.data]
-    delbtn.splice(i,1)
+    const delbtn = [...this.state.data]
+    delbtn.splice(i, 1)
     this.setState({
-      data:delbtn
+      data: delbtn
     })
     this.clearForm()
   }
 
-  updateButtonEvent=()=>{
-    const updateUser=[...this.state.data]
-    updateUser[this.state.editIndex]=this.state.user
+  updateButtonEvent = () => {
+    const updateUser = [...this.state.data]
+    updateUser[this.state.editIndex] = this.state.user
     this.setState({
-      data:updateUser,editIndex:null
+      data: updateUser, editIndex: null
     })
     this.clearForm()
   }
-  clearForm=()=>{
-    let clear={...this.state.user}
-    clear={
-      fname:"",lname:"",gender:"",url:""
+  clearForm = () => {
+    let clear = { ...this.state.user }
+    clear = {
+      fname: "", lname: "", gender: "", url: ""
     }
-  this.setState({user:clear})
+    this.setState({ user: clear })
   }
 
   render() {
     return (
       <div>
         <div className="divdata1">
-        <form>
-        <h3>Registration Form 3</h3>
-          <div>
-            <label>First Name</label>
-            <input type="text"
-            name='fname'
-            value={this.state.user.fname}
-            onChange={this.onChangeEvent} />
-          </div>
-          <div>
-            <label>Last Name</label>
-            <input type="text"
-            name='lname'
-            value={this.state.user.lname}
-            onChange={this.onChangeEvent}  />
-          </div>
-          <div>
-            <label>Gender</label>
-            <select
-            name='gender'
-            onChange={this.onChangeEvent} >
-              <option value="">Select..</option>
-              <option  name="male"   value="male">Male</option>
-              <option  name="female" value="female">Female</option>
-              <option  name="others" value="other">Other</option>
-            </select>
-          </div>
-          <div>
-            <label>Website</label>
-            <input type="url" 
-            name='url'
-            value={this.state.user.url}
-            onChange={this.onChangeEvent} />
-          </div>
-          <div>
-            {this.state.editIndex!=null?(
-              <button type='button' onClick={this.updateButtonEvent}>Update</button>
-            ):(
-              <button type='button' onClick={this.submitButton}>Regiser</button>
-            )}
-           
-          </div>
-        </form>
+          <form>
+            <h3>Registration Form 3</h3>
+            <div>
+              <label>First Name</label>
+              <input type="text"
+                name='fname'
+                value={this.state.user.fname}
+                onChange={this.onChangeEvent} />
+            </div>
+            <div>
+              <label>Last Name</label>
+              <input type="text"
+                name='lname'
+                value={this.state.user.lname}
+                onChange={this.onChangeEvent} />
+            </div>
+            <div>
+              <label>Gender</label>
+              <select
+                name='gender'
+                onChange={this.onChangeEvent} >
+                <option value="">Select..</option>
+                <option name="male" value="male">Male</option>
+                <option name="female" value="female">Female</option>
+                <option name="others" value="other">Other</option>
+              </select>
+            </div>
+            <div>
+              <label>Website</label>
+              <input type="url"
+                name='url'
+                value={this.state.user.url}
+                onChange={this.onChangeEvent} />
+            </div>
+            <div>
+              {this.state.editIndex != null ? (
+                <button type='button' onClick={this.updateButtonEvent}>Update</button>
+              ) : (
+                <button type='button' onClick={this.submitButton}>Regiser</button>
+              )}
+
+            </div>
+          </form>
         </div>
         <div className="divdata2">
           <table border={1} cellPadding={10} cellSpacing={0}>
@@ -119,17 +119,17 @@ class RegistrationForm3 extends Component {
               </tr>
             </thead>
             <tbody>
-              {this.state.data.map((val,i)=>{
+              {this.state.data.map((val, i) => {
                 return (
                   <tr key={i}>
                     <td>{val.fname}</td>
                     <td>{val.lname}</td>
                     <td>{val.gender}</td>
                     <td>{val.url}</td>
-                    <td><button type='button' onClick={()=>{
-                      this.editButtonEvent(val,i)
+                    <td><button type='button' onClick={() => {
+                      this.editButtonEvent(val, i)
                     }}>Edit</button></td>
-                    <td><button type='button' onClick={()=>{
+                    <td><button type='button' onClick={() => {
                       this.deleteButtonEvent(i)
                     }}>Delete</button></td>
                   </tr>
