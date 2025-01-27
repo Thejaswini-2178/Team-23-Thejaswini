73ac6d8f2b2bcf152f25bec69eebf584a811010e
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-01-27 23:52:16 +0530

    React-social-media-post

---
+++ ReactJS/redux/my-redux-app/src/redux/socialMediaPost.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const socialMediaPostSlice=createSlice({
+    name:"socialMediaPost",
+    initialState:["post1","post2","post3"],
+    reducers:{},
+})
+export default socialMediaPostSlice.reducer;
