7b20fcd1d66d1f17928d0655e04ac1b666b64aba
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-01-27 23:42:34 +0530

    React-Ecommerce-cart

--- ReactJS/redux/my-redux-app/src/redux/store.js
+++ ReactJS/redux/my-redux-app/src/redux/store.js
@@ -1,7 +1,15 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import { ecommerceSlice } from "./ecommerce";
+// export const store=configureStore({
+//     reducer:{
+//         ecommerce:ecommerceSlice
+//     }
+// })
 import { configureStore } from "@reduxjs/toolkit";
 import { ecommerceSlice } from "./ecommerce";
-export const store=configureStore({
-    reducer:{
-        ecommerce:ecommerceSlice
-    }
-})
+
+export const store = configureStore({
+    reducer: {
+        ecommerce: ecommerceSlice.reducer, // Use the `reducer` property of the slice
+    },
+});
