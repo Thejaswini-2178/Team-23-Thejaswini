d9b0100b625e119f165c8b61c3f20a41546cb0d2
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-01-28 16:01:00 +0530

    Redex-Store

--- ReactJS/redux/my-redux-app/src/redux/store.js
+++ ReactJS/redux/my-redux-app/src/redux/store.js
@@ -7,9 +7,30 @@
 // })
 import { configureStore } from "@reduxjs/toolkit";
 import { ecommerceSlice } from "./ecommerce";
+import { socialMediaPostSlice } from "./socialMediaPost";
+import { onlineQuizSlice } from "./onlineQuiz";
+import { taskManagerSlice } from "./taskManager";
+import { userProfileSlice } from "./userProfile"
+import { addressBookSlice } from "./addressBooking"
+import { librarySlice } from "./libraryManagement"
+import { movieTicketSlice } from "./movieTicktes"
+import { inventarySice } from "./inventaryManagement";
+import { expenseSlice } from "./expenseTraker";
+
+
+
 
 export const store = configureStore({
     reducer: {
         ecommerce: ecommerceSlice.reducer, // Use the `reducer` property of the slice
+        socialMediaPost:socialMediaPostSlice.reducer,
+        onlineQuiz:onlineQuizSlice.reducer,
+        taskManager:taskManagerSlice.reducer,
+        userProfile:userProfileSlice.reducer,
+        addressBook:addressBookSlice.reducer,
+        library:librarySlice.reducer,
+        movieTicket:movieTicketSlice.reducer,
+        inventary:inventarySice.reducer,
+        expense:expenseSlice.reducer
     },
 });
