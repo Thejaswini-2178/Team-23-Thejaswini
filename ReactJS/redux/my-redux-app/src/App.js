de3c132ca29798084105c3574f1bcffe098cefb8
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-01-28 16:00:30 +0530

    Redux-App

--- ReactJS/redux/my-redux-app/src/App.js
+++ ReactJS/redux/my-redux-app/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+ import './App.css';
+import AddressBooking_7 from './Components/AddressBooking_7';
 import Ecommerce_1 from './Components/Ecommerce_1';
+import ExpenseTraker_10 from './Components/ExpenseTraker_10';
+import InventaryManageMent_9 from './Components/InventaryManageMent_9';
+import LibraryManagement_6 from './Components/LibraryManagement_6';
+import MovieTicktesBook_8 from './Components/MovieTicktesBook_8';
+import OnlineQuiz_3 from './Components/OnlineQuiz_3';
+import SocialMedia_Post_2 from './Components/SocialMedia_Post_2';
+import TaskManager_4 from './Components/TaskManager_4';
+import UserProfile_5 from './Components/UserProfile_5';
 
 function App() {
   return (
     <div className="App">
-      <Ecommerce_1 />
+      <Ecommerce_1 /><hr/>
+      <SocialMedia_Post_2/><hr/>
+      <OnlineQuiz_3/><hr/>
+      <TaskManager_4/><hr/>
+      <UserProfile_5/><hr/>
+      <LibraryManagement_6/><hr/>
+      <AddressBooking_7/><hr/>
+      <MovieTicktesBook_8/><hr/>
+      <InventaryManageMent_9/><hr/>
+      <ExpenseTraker_10/>
     </div>
   );
 }
