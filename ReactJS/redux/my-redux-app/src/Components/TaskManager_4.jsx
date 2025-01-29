3ce31045f44fb423e7da4cddae9a30edabf2fca2
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-01-29 15:31:35 +0530

    Redux-Expense-Traker

--- ReactJS/redux/my-redux-app/src/Components/TaskManager_4.jsx
+++ ReactJS/redux/my-redux-app/src/Components/TaskManager_4.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+// import { useSelector } from 'react-redux'
 
 const TaskManager_4 = () => {
-    const taskManager=useSelector((state)=>state.taskManager)
+      // const taskManager = useSelector((state) => state.taskManager)
   return (
     <div>
-        <h2>TaskManager_4</h2>
-        <ul>
-            {taskManager.map((task)=>{
-                return <li>{task}</li>
-            })}
-        </ul>
+      Welcome to taskManager
+      {/* {taskManager.map} */}
     </div>
   )
 }
