e920325ff9e5e7453cbe0a9d280dc7d0210b304d
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-01-30 22:20:55 +0530

    Redux

--- ReactJS/redux/my-redux-app/src/Components/SocialMedia_Post_2.jsx
+++ ReactJS/redux/my-redux-app/src/Components/SocialMedia_Post_2.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const SocialMedia_Post_2 = () => {
-    const socialMediaPost = useSelector((state) => state.socialMediaPost)
+  const socialMediaPost = useSelector((state) => state.socialMediaPost)
   return (
     <div>
-        <h2>
-            Welcome to SocialMedia_Post_2
-        </h2>
-        <ul>
-          {socialMediaPost.map((post)=>{
-            return <li>{post}</li>
-          })}
-        </ul>
+      <h2>
+        Welcome to SocialMedia_Post_2
+      </h2>
+      <ul>
+        {socialMediaPost.map((post) => {
+          return <li>{post}</li>
+        })}
+      </ul>
     </div>
   )
 }
--- ReactJS/redux/my-redux-app/src/redux/expenseTraker.js
+++ ReactJS/redux/my-redux-app/src/redux/expenseTraker.js
@@ -1,7 +1,7 @@
-import {createSlice  } from "@reduxjs/toolkit";
-const expenseSlice=createSlice({
-    name:"expense",
-    initialState:["product1","product2","product3"],
-    reducers:{}
+import { createSlice } from "@reduxjs/toolkit";
+const expenseSlice = createSlice({
+    name: "expense",
+    initialState: ["product1", "product2", "product3"],
+    reducers: {}
 })
 export default expenseSlice.reducer
