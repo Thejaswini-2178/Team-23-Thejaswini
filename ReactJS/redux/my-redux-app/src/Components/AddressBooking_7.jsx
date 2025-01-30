d9eb6cb2b80d5d60cc964960a3d103f79f68b8ef
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-01-30 22:18:46 +0530

    Redux-AddressBook

--- ReactJS/redux/my-redux-app/src/Components/AddressBooking_7.jsx
+++ ReactJS/redux/my-redux-app/src/Components/AddressBooking_7.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const AddressBooking_7 = () => {
-    const addressBook=useSelector((state)=>state.addressBook)
+  const addressBook = useSelector((state) => state.addressBook)
   return (
     <div>
-        <h2>AddressBooking_7</h2>
-        <ul>
-        {addressBook.map((a)=>{
-                    return <li >{a}</li>
-                })}
-        </ul>
+      <h2>AddressBooking_7</h2>
+      <ul>
+        {addressBook.map((a) => {
+          return <li >{a}</li>
+        })}
+      </ul>
     </div>
   )
 }
