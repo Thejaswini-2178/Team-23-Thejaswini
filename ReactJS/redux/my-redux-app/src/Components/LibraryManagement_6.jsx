152c557ea02aa309d7396c7fe814b44e7b714a1a
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-01-30 22:19:07 +0530

    Redux-Lybrarymanagement

--- ReactJS/redux/my-redux-app/src/Components/LibraryManagement_6.jsx
+++ ReactJS/redux/my-redux-app/src/Components/LibraryManagement_6.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const LibraryManagement_6 = () => {
-    const library=useSelector((state)=>state.library)
+  const library = useSelector((state) => state.library)
   return (
     <div>
-        <h2>LibraryManagement_6</h2>
-        <ul>
-            {library.map((library)=>{
-                return <li>{library}</li>
-            })}
-        </ul>
+      <h2>LibraryManagement_6</h2>
+      <ul>
+        {library.map((library) => {
+          return <li>{library}</li>
+        })}
+      </ul>
     </div>
   )
 }
