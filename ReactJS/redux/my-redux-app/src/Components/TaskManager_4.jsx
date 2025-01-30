9ab55e44a07604cfb9a205213a719b3da8c9367c
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-01-30 22:26:44 +0530

    Redux-Taskmanager

--- ReactJS/redux/my-redux-app/src/Components/TaskManager_4.jsx
+++ ReactJS/redux/my-redux-app/src/Components/TaskManager_4.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-// import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const TaskManager_4 = () => {
-  // const taskManager = useSelector((state) => state.taskManager)
+  const taskManager = useSelector((state) => state.taskManager)
   return (
     <div>
       Welcome to taskManager
