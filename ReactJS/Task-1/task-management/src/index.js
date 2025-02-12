685a6e2300206ea8babe6fd8bef46662bc1d2c21
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-02-12 12:44:33 +0530

    Task-Management-index

--- ReactJS/Task-1/task-management/src/index.js
+++ ReactJS/Task-1/task-management/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import App from './App';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client"; // ✅ Use 'react-dom/client' instead of 'react-dom'
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root")); // ✅ Correct way in React 18
+
 root.render(
     <Provider store={store}>
         <App />
