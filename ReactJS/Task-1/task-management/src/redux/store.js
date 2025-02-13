9f25faaaa621c7362b91928b0dadcb3f2661ae5e
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-02-13 23:10:04 +0530

    React-Task

--- ReactJS/Task-1/task-management/src/redux/store.js
+++ ReactJS/Task-1/task-management/src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import taskReducer from './taskSlice';
 import authReducer from './authSlice';
 
+
 const store = configureStore({
     reducer: {
         tasks: taskReducer,
