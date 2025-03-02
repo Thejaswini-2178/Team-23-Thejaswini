cdaf66c32085c0e20b16a16abb264f117eca2b19
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-03-02 21:41:27 +0530

    Hooks

--- ReactJS/hooks/hooks/src/App.js
+++ ReactJS/hooks/hooks/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Components/useState';
+import { Hook1 } from './Components/hook1';
+import Timer from './Components/useeffect';
+import ExpensiveCalculation from './Components/useMemo';
+import FocusInput from './Components/useRef';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter/>
+      <Hook1/>
+      <Timer/>
+      <ExpensiveCalculation/>
+      <FocusInput/>
+      <FocusEvent/>
     </div>
   );
 }
