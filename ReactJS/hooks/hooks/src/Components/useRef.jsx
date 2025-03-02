7664404837c64c425966cc9f6c8d42dd93de8f41
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-03-02 21:41:03 +0530

    UseRef

---
+++ ReactJS/hooks/hooks/src/Components/useRef.jsx
@@ -0,0 +1,11 @@
+import { useRef, useEffect } from "react";
+
+export default function FocusInput() {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return <input ref={inputRef} placeholder="Type here..." />;
+}
