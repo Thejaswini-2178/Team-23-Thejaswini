44705c19cda43a7725dcb5ddd07a79c14e6f6577
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-03-02 21:40:29 +0530

    Useeffect

---
+++ ReactJS/hooks/hooks/src/Components/useeffect.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+export default function Timer() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <p>Timer: {count}</p>;
+}
