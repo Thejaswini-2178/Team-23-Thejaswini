9a63f7adbbc7e54365ff6a91bfe7594f1edb9576
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-03-03 23:17:12 +0530

    usememo

--- ReactJS/hooks/hooks/src/Components/useMemo.jsx
+++ ReactJS/hooks/hooks/src/Components/useMemo.jsx
@@ -3,7 +3,6 @@ import { useState, useMemo } from "react";
 export default function ExpensiveCalculation() {
   const [count, setCount] = useState(0);
   const [num, setNum] = useState(10);
-
   const squaredNumber = useMemo(() => {
     console.log("Calculating...");
     return num * num;
