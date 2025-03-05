f1c5e1a3717c3564ee15015980fe38918cdbbec7
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-03-05 21:39:43 +0530

    updatingPhase

--- ReactJS/react-lifecycle/src/lifecyclemethods/UpdatingPhaseMethod.jsx
+++ ReactJS/react-lifecycle/src/lifecyclemethods/UpdatingPhaseMethod.jsx
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
 
 export default class UpdatingPhaseMethod extends Component {
-    constructor(Props){
-        super(Props)
-        this.state={id:25}
-        console.log(   ""   )
-        console.log("Constructed is called.....")
-    }
-    static getDerivedStateFromProps(props,state){
-        console.log("static getDerivedStateFromProps is called...");
-        return null;
-    }
-    shouldComponentUpdate(){
-        console.log("shouldComponentUpdate is called.....");
-        return true;
-    }
-    getSnapshotBeforeUpdate(){
-        console.log("getSnapshotBeforeUpdate is called...")
-        return true;
-    }
-    componentDidUpdate(){
-        console.log("componentDidUpdate is called...")
-        return true;
-    }
+  constructor(Props) {
+    super(Props)
+    this.state = { id: 25 }
+    console.log("")
+    console.log("Constructed is called.....")
+  }
+  static getDerivedStateFromProps(props, state) {
+    console.log("static getDerivedStateFromProps is called...");
+    return null;
+  }
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate is called.....");
+    return true;
+  }
+  getSnapshotBeforeUpdate() {
+    console.log("getSnapshotBeforeUpdate is called...")
+    return true;
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate is called...")
+    return true;
+  }
   render() {
     console.log("render is called....")
     return (
       <div>
-        <button onClick={()=>this.setState({count:this.state.id+1})}>CLICK</button>
+        <button onClick={() => this.setState({ count: this.state.id + 1 })}>CLICK</button>
       </div>
     )
   }
